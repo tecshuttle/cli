1ea33284ee06929cf7c42ba0c10895970d6b5deb
tecshuttle <tecshuttle@gmail.com>
2020-07-04 10:31:55 +0800

    新建项目，使用commander、inquirer收集配置信息。

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
 #!/usr/bin/env node
-function run(argv) {
-  if (argv[0] === '-v' || argv[0] === '--version') {
+var program = require('commander');
+var inquirer = require('inquirer');
+var Rx = require('rxjs');
 
-    console.log('  version is 0.0.1');
+program
+  .version('0.0.2', '-v, --version');
 
-  } else if (argv[0] === '-h' || argv[0] === '--help') {
+program
+  .command('create <project_name>')
+  .option('-t, --type', 'project type')
+  .action(function (project_name, cmd) {
+    if (cmd.type) {
+      let output = 'create '
+        + (cmd.type ? 'admin type project ' : '')
+        + 'tplhk-' + project_name + '-ui.'
+      console.log(output);
+    } else {
+      inquirer.prompt([
+        {
+          type: 'rawlist',
+          name: 'type',
+          message: '选择项目类型',
+          choices: [
+            { name: '管理后台', value: 'admin' },
+            { name: '网站门户', value: 'website' },
+            { name: '产品门户', value: 'product' }
+          ],
+          default: 0 // 默认值为列表项编号，起始为 0
+        }
+      ]).then((answer) => {
+        console.log('结果为: ' + answer.type);
+        inquirer.prompt([
+          {
+            type: 'checkbox',
+            name: 'module',
+            message: '选择模块功能',
+            choices: [
+              { name: '登入', value: 'login' },
+              { name: '角色', value: 'role' },
+              { name: '用户', value: 'user' }
+            ],
+            default: ['login', 'role', 'user'] // 默认值为列表项编号，起始为 0
+          }
+        ]).then((answer) => {
+          console.log('结果为: ');
+          console.log(answer);
 
-    console.log('  usage:\n');
-    console.log('  -v --version [show version]');
 
-  }
-}
+          let prompList = [{
+            type: 'input',
+            name: 'platform_code',
+            message: '平台代码（示例：web_cms_hk）'
+          }, {
+            type: '',
+            name: 'secret',
+            message: '平台密码'
+          }, {
+            type: 'input',
+            name: 'apiUrl',
+            message: '接口地址',
+          }];
 
-run(process.argv.slice(2));
+          inquirer.prompt(prompList).then((answers) => {
+            console.log(answers);
+          });
+        });
+      });
+    }
+  })
+
+program.parse(process.argv);
+
+//end
